fd33dd6269e6389d2a100862eaa60bccc765bec7
chirag baghra <chiragbaghra1990@gmail.com>
2019-11-23 13:33:56 +0530

    hello

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const menubtn=document.querySelector('.menu-btn');
+const menu=document.querySelector('.menu');
+const menuNav=document.querySelector('.menu-nav');
+const menuBranding=document.querySelector('.menu-branding');
+const navItems=document.querySelectorAll('.nav-item');
+
+let showmenu=false;
+menubtn.addEventListener('click',toggleMenu);
+function toggleMenu(){
+    if(!showmenu){
+        menubtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item=>item.classList.add('show'));
+        showmenu=true;
+    }
+    else{
+        menubtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item=>item.classList.remove('show'));
+        showmenu=false;
+    }
+}
+
+
